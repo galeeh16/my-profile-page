8285d2032a8cf0e2066717068f5769bec90ce67e
Galih Anggoro Jati <anggorogalih8@gmail.com>
2023-08-02 17:02:13 +0700

    change nuxt.config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,10 +13,10 @@ export default defineNuxtConfig({
         lang: 'en'
       },
       meta: [
-        // {
-        //   name: 'viewport',
-        //   content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
-        // },
+        {
+          name: 'viewport',
+          content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'
+        },
         {
           name: 'description',
           content: 'Profile Galih Anggoro Jati'
